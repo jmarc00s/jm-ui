c22bcb325179eec639810920a82de64ce71f93ba
João Marcos <jmarcossferreira@hotmail.com>
2023-01-23 08:47:34 -0300

    Remove component from app

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,6 @@ function App() {
       <div className="navbar border-b-2 border-base-200 px-4">
         <span className="font-semibold">React + daisyui + CVA</span>
       </div>
-
-      <div className="flex">
-        <Button variant="primary">Teste</Button>
-      </div>
     </main>
   );
 }
