dc6e6a80085edb791e3661474d22e56a10687b22
João Marcos <jmarcossferreira@hotmail.com>
2023-01-31 08:24:33 -0300

    Fix counterAvatar test

--- src/components/AvatarGroup/components/CounterAvatar/CounterAvatar.spec.tsx
+++ src/components/AvatarGroup/components/CounterAvatar/CounterAvatar.spec.tsx
@@ -5,6 +5,6 @@ import { CounterAvatar } from '.';
 describe('<CounterAvatar />', () => {
   it('should render counter avatar', () => {
     render(<CounterAvatar counter={10} size="sm" />);
-    expect(screen.getByText(10)).toBeVisible();
+    expect(screen.getByText('+10')).toBeVisible();
   });
 });
